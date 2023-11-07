3ebf15500613112180c2c5c398ca01c52ddac3f2
MilanN <n.milan89@gmail.com>
2023-11-07 22:08:29 +0600

    add files: deep> match_case3

---
+++ Deep-10/match_case3-10_6/task-5.py
@@ -0,0 +1,14 @@
+def parse_json(data):
+    match data:
+        case {'access': True, 'data': [_, {'login': str() as login, 'email': str() as email}, *_]}:
+            return login, email
+
+        case {'id': ids, 'data': [_, {'login': login}, _, _]}:
+            return ids, login
+
+    return None
+
+
+json_data = {'id': 2, 'access': True, 'data': ['26.05.2023', {'login': '1234', 'email': 'xxx@mail.com'}, 2000, 56.4]}
+
+print(parse_json(json_data))
