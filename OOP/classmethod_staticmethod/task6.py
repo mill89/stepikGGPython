cd790b0e40d802e46e43b16e6c3f2dbaea16e59b
milan <n.milan1989@gmail.com>
2024-01-30 16:38:24 +0600

    дописал task6.py

--- OOP/classmethod_staticmethod/task6.py
+++ OOP/classmethod_staticmethod/task6.py
@@ -1,8 +1,8 @@
 class Loader:
     @staticmethod
-    def parse_format(string, factory):
-        seq = fac.build_sequence()
-        for sub in string.split(','):
+    def parse_format(string_: str, factory: object) -> list[int]:
+        seq = factory.build_sequence()
+        for sub in string_.split(','):
             item = factory.build_number(sub)
             seq.append(item)
 
@@ -10,14 +10,19 @@ class Loader:
 
 
 class Factory:
+    #  создает, возвращает пустой список
     @staticmethod
-    def build_sequence():
-        pass
+    def build_sequence() -> list:
+        return []
 
+    # преобразует строку в целое число
     @staticmethod
-    def build_number(string):
-        pass
+    def build_number(string: str) -> int:
+        return int(string)
 
 
 res1 = Loader.parse_format('4, 5 , -6', Factory)
 res2 = Loader.parse_format("1, 2, 3, -5, 10", Factory)
+
+print(res1)
+print(res2)
