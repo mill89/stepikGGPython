1891bf91b6db445004ffd58e0d00282b4ad549c5
Milan Nasrytdinov <milan.nasrytdinov@gmail.com>
2024-02-27 21:05:06 +0600

    trial1.py

--- OOP/trial1.py
+++ OOP/trial1.py
@@ -1,48 +1,59 @@
-# для описания работы серверов в сети
+
+class Data:
+    # для описания пакета информации
+    def __init__(self, data: str, ip: int) -> None:
+        self.data = data  # передаваемые данные (строка)
+        self.ip = ip  # IP-адрес назначения
+
+
 class Server:
-    def __init__(self, ip):
+    # для описания работы серверов в сети
+    def __init__(self, ip: int) -> None:
         self.buffer = []  # список принятых пакетов (объекты класса Data, изначально пустой)
         self.ip = ip  # IP-адрес текущего сервера
 
-    def send_data(self, data):
+    def send_data(self, data: str) -> object:
         # для отправки информационного пакета data (объекта класса Data) с указанным IP-адресом получателя
         # (пакет отправляется роутеру и сохраняется в его буфере - локальном свойстве buffer)
-        pass
+        obj = Data(data, self.ip)
+        return obj
 
-    def get_data(self):
+
+    def get_data(self) -> list:
         # возвращает список принятых пакетов (если ничего принято не было, то возвращается пустой список)
         # и очищает входной буфер
-        pass
+        self.buffer.append(...)
 
-    def get_ip(self):
+    def get_ip(self) -> int:
         # возвращает свой IP-адрес
-        pass
+        return self.ip
 
 
-# для описания работы роутеров в сети
+
 class Router:
-    def __init__(self):
+    # для описания работы роутеров в сети
+    def __init__(self) -> None:
         # список для хранения принятых от серверов пакетов (объектов класса Data)
         self.buffer = []
-    def link(self, server):
-        # для присоединения сервера server (объекта класса Server) к роутеру
-        pass
 
-    def unlink(self, server):
+    def link(self, server: int):
+        # для присоединения сервера server (объекта класса Server) к роутеру
+        obj = Server(server)
+        self.buffer.append(obj.send_data())
+
+    def unlink(self, server: int) -> None:
         # для отсоединения сервера server (объекта класса Server) от роутера
-        pass
+        obj = Server(server)
+        self.buffer.remove(obj)
 
     def send_data(self):
         # для отправки всех пакетов (объектов класса Data) из буфера роутера соответствующим серверам
         # (после отправки буфер должен очищаться)
-        pass
+        for pack in self.buffer:
+            pack.send_data()
+        self.buffer.clear()
 
 
-# для описания пакета информации
-class Data:
-    def __init__(self, data, ip):
-        self.data = data  # передаваемые данные (строка)
-        self.ip = ip  # IP-адрес назначения
 
 
 
