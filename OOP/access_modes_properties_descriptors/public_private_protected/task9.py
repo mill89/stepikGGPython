3dcc7dee303ccb01aabb684ed00c4fb7f1c5a438
MilanN <n.milan89@gmail.com>
2024-03-19 23:54:47 +0600

    task9.py pub_priv_prot

--- OOP/access_modes_properties_descriptors/public_private_protected/task9.py
+++ OOP/access_modes_properties_descriptors/public_private_protected/task9.py
@@ -1,25 +1,25 @@
 class ObjList:
-    def __init__(self, data: str, next=None, prev=None) -> None:
-        self.__next = next  # ссылка на следующий объект
-        self.__prev = prev  # ссылка на предыдущий объект
+    def __init__(self, data: str) -> None:
+        self.__next = None  # ссылка на следующий объект
+        self.__prev = None  # ссылка на предыдущий объект
         self.__data = data  # строка с данными
 
-    def set_next(self, obj):
+    def set_next(self, obj: object) -> None:
         self.__next = obj
 
-    def set_prev(self, obj):
+    def set_prev(self, obj: object) -> None:
         self.__prev = obj
 
-    def get_next(self):
+    def get_next(self) -> object:
         return self.__next
 
-    def get_prev(self):
+    def get_prev(self) -> object:
         return self.__prev
 
-    def set_data(self, data):
+    def set_data(self, data: str) -> None:
         self.__data = data
 
-    def get_data(self):
+    def get_data(self) -> str:
         return self.__data
 
 
@@ -28,24 +28,31 @@ class LinkedList:
         self.head = None  # ссылка на первый объект
         self.tail = None  # ссылка на последний объект
 
-    def add_obj(self, obj: ObjList):
+    def add_obj(self, obj: ObjList) -> None:
         '''Добавление нового объекта obj класса ObjList в конец связного списка'''
-        if self.head == None:
-            self.tail = obj
-        else:
-            obj.set_prev(self.head)
-            self.tail = obj
+        if self.tail:
+            self.tail.set_next(obj)
+        obj.set_prev(self.tail)
+        self.tail = obj
+        if not self.head:
+            self.head = obj
 
-
-    def remove_obj(self):
+    def remove_obj(self) -> None:
         '''Удаление последнего объекта связного списка'''
-        self.tail = None
+        if self.tail is None:
+            return
+        prev = self.tail.get_prev()
+        if prev:
+            prev.set_next(None)
 
+        self.tail = prev
+        if self.tail is None:
+            self.head = None
 
-    def get_data(self):
+    def get_data(self) -> list[str]:
         '''Получение списка из строк локального свойства __data всех объектов связного списка'''
         l = []
-        while self.tail.get_next != None:
+        while self.head:
             l.append(self.head.get_data())
             self.head = self.head.get_next()
 
