2986f7ca90872292399ff03f218686ea52ce3d1c
Milan Nasrytdinov <milan.nasrytdinov@gmail.com>
2024-04-29 16:03:30 +0600

    add property> task4.py

--- OOP/access_modes_properties_descriptors/property_@property/task4.py
+++ OOP/access_modes_properties_descriptors/property_@property/task4.py
@@ -1,18 +1,6 @@
 class Car:
-    def __init__(self, model):
-        self.__model = model
-        self.verify_model(self.model)
-        self.verify_str(self.model)
-
-    @classmethod
-    def verify_model(cls, model):
-        if type(model) is not str:
-            raise TypeError('Должна быть сторока')
-
-    @classmethod
-    def verify_str(cls, model):
-        if len(model) < 2 or len(model) > 100:
-            raise TypeError('Неверная длина строки [2; 100]')
+    def __init__(self):
+        self.__model = None
 
     @property
     def model(self):
@@ -20,12 +8,13 @@ class Car:
 
     @model.setter
     def model(self, model):
-        self.__model = model
+        if type(model) is str and 2 <= len(model) <= 100:
+            self.__model = model
 
 
 if __name__ == '__main__':
-    car = Car('kjjhjhjh')
+    car = Car()
     print(car.model)
-    car.model = 'mazda'
+    car.model = 'milan'
 
     print(car.model)
