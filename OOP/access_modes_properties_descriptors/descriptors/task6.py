c2a30eea5dca33bf186405bb7e6cdbbf45d8ce9d
Milan Nasrytdinov <n.milan1989@gmail.com>
2024-05-30 15:42:28 +0600

    task6.py descriptors

--- OOP/access_modes_properties_descriptors/descriptors/task6.py
+++ OOP/access_modes_properties_descriptors/descriptors/task6.py
@@ -6,7 +6,7 @@ class FloatValue:
         return instance.__dict__[self.name]
 
     def __set__(self, instance, value):
-        if instance(value, float):
+        if isinstance(value, float):
             instance.__dict__[self.name] = value
         else:
             raise TypeError('присваивать можно только вещественный тип данных')
@@ -17,9 +17,16 @@ class Cell:
 
 
 class TableSheet:
-
     def __init__(self, N, M):
-        self.cells = list(range(N, M))
+        self.cells = [[Cell() for _ in range(M)] for _ in range(N)]
 
 
-    def create_table(self):
+if __name__ == '__main__':
+    table = TableSheet(5, 3)
+    n = 1.0
+    for x in range(5):
+        for y in range(3):
+            table.cells[x][y] = n
+            n += 1
+
+    print(table.cells)
