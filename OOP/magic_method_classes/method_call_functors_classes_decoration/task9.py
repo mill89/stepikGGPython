303d331494b7cd6ed33577a29a46864070665337
Milan Nasrytinov <n.milan1989@gmail.com>
2024-06-29 15:04:47 +0600

    add task9, 10.py > call

---
+++ OOP/magic_method_classes/method_call_functors_classes_decoration/task9.py
@@ -0,0 +1,12 @@
+class InputDigits:
+    '''Декорирование стандартной функции input'''
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        return list(map(int, self.func().split()))
+
+
+input_dg = InputDigits(input)
+res = input_dg()
+print(res)
